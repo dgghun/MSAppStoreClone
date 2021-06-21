1f9a070aa905752733c4715355872bf7d8e1db6d
dgghun <gabriel3052000@yahoo.com>
2021-06-21 12:19:07 -0700

    added hamburger list logic

--- MSAppStoreClone/UserControls/HamburgerMenuViews/HamburgerMenuApp.xaml.cs
+++ MSAppStoreClone/UserControls/HamburgerMenuViews/HamburgerMenuApp.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MiscUtil;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,38 @@ namespace MSAppStoreClone.UserControls.HamburgerMenuViews
     /// </summary>
     public partial class HamburgerMenuApp : UserControl
     {
+        public List<string> AppNames;
+        public List<string> AppTypes;
+        public string AppName;
+        public DateTime Purchased;
+        public string Type;
+
+
         public HamburgerMenuApp()
         {
             InitializeComponent();
+
+            AppTypes = new List<string>()
+            {
+                "App",
+                "Game",
+                "Movie",
+                "Avatar",
+            };
+
+            List<string> filepaths = Directory.GetFiles(Environment.CurrentDirectory
+                + @"\..\..\Images\MiniIcons", "*.png").ToList<string>();  //Get list of icon image paths
+            FileInfo myRandomFile = new FileInfo(filepaths[StaticRandom.Next(filepaths.Count)]);
+            AppImage.Source = new BitmapImage(new Uri(myRandomFile.FullName, UriKind.RelativeOrAbsolute));
+            AppNameLabel.Content = (new CultureInfo("en-us", false).TextInfo).ToTitleCase
+            (
+                AppImage.Source.ToString().Split('/').Last().Split('.').First()
+                .Split('-').Last().Split('.').First()
+            );
+            AppName = AppNameLabel.Content.ToString();
+            Type = AppTypes[StaticRandom.Next(AppTypes.Count)];
+            Purchased = new DateTime(2021, 1, StaticRandom.Next(1, DateTime.Now.Day + 1));
+            PurchasedLabel.Content = "Purchased " + Purchased.ToString("d");
         }
     }
 }
